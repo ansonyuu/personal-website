c9b6292b63c8717e2dfdbd6fd71f752a75b45434
Anson Yu <ansonyu24@gmail.com>
2020-09-07 12:42:09 -0400

    Fixed responsiveness of landing page

--- main.js
+++ main.js
@@ -25,22 +25,22 @@ const navSlide = () => {
 
 navSlide();
 
-// Making the home page elements change on scroll
-document.querySelectorAll('.scene').forEach((elem) => {
+// // Making the home page elements change on scroll
+// document.querySelectorAll('.scene').forEach((elem) => {
 	
-	const modifier = elem.getAttribute('data-modifier')
+// 	const modifier = elem.getAttribute('data-modifier')
 	
-	basicScroll.create({
-		elem: elem,
-		from: 0,
-		to: 1000,
-		direct: true,
-		props: {
-			'--translateY': {
-				from: '0',
-				to: `${ 10 * modifier }px`
-			}
-		}
-	}).start()
+// 	basicScroll.create({
+// 		elem: elem,
+// 		from: 0,
+// 		to: 1000,
+// 		direct: true,
+// 		props: {
+// 			'--translateY': {
+// 				from: '0',
+// 				to: `${ 10 * modifier }px`
+// 			}
+// 		}
+// 	}).start()
 		
-})
+// })
